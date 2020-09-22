3f5cacbf3d2a84f7e39cd4c53b3ba3ee13435758
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-09-21 20:18:39 -0400

    20200921

---
+++ JavaScript_Programing/jsScratchPad.js
@@ -0,0 +1,9 @@
+const meals = [
+    {description: 'Breakfast', calories: 180, date: new Date(2020, 0, 1)},
+    {description: 'Snack', calories: 265, date: new Date(2020, 0, 1)},
+];
+
+
+const meal1 = meals[0];
+
+console.log(meal1);
