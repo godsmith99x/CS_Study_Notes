fad5b1a291dc228d7fd3b1f6b0c7b26d500907bf
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-09-22 15:21:54 -0400

    20200922

--- JavaScript_Programing/jsScratchPad.js
+++ JavaScript_Programing/jsScratchPad.js
@@ -1,32 +1,17 @@
-//remove data when copying to new object literal with spread operator
-const meal = {
-    id: 1,
-    description: 'Breakfast',
+//use spread syntax to update an immutable array
+const meals = [
+    {id: 1, description: 'Breakfast', calories: 420},
+    {id: 2, description: 'Lunch', calories: 520},
+];
+
+const newMeal = {
+    id: 3,
+    description: 'Snack',
+    calories: 180,
 };
 
-const updateMeal = {
-    //... before name means to inject data into new item but if any of the fields match, those in the new item take precedence 
-    ...meal,
-    //injects a calories field in meal object literal
-    calories: 600,
-    //takes all data from meal object literal but uses this description instead
-    description: 'Brunch',
-};
+//... before meals means copy all the data from meals into new object literal with newMeal info as well
+const updatedMeals = [...meals, newMeal];
 
-console.log(meal, updateMeal);
-
-//this is a verbose way to just get the two fields we want but more elegant to use destructuring
-//const description = updateMeal.description;
-//const calories = updateMeal.calories;
-
-//example of destructuring
-const {description, calories} = updateMeal;
-
-console.log(description, calories);
-
-//destructuring with rest(...) syntax
-//creates a new object literal with all data from updateMeal without the id field
-const {id, ...mealWithoutId} = updateMeal;
-
-console.log(mealWithoutId);
+console.log(meals, updatedMeals);
 
