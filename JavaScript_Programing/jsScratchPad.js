33624e8cec6d465a3b6e0b9c393515d781ea5ef1
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-09-23 13:46:40 -0400

    20200923

--- JavaScript_Programing/jsScratchPad.js
+++ JavaScript_Programing/jsScratchPad.js
@@ -1,4 +1,4 @@
-//use spread syntax to update an immutable array
+//use map function to update an item in an immutable array
 const meals = [
     {id: 1, description: 'Breakfast', calories: 420},
     {id: 2, description: 'Lunch', calories: 520},
@@ -15,3 +15,27 @@ const updatedMeals = [...meals, newMeal];
 
 console.log(meals, updatedMeals);
 
+//creates a new object literal, called updatedMealsDescription, by iterating over each index in the updatedMeals array and running the updateDescription function at each point
+const updatedMealsDescription = updatedMeals.map(updateDescription);
+
+
+//looks at the id of the object literal in the array at each index of a matrix, and if the id is 2, it updates the description and copies it into a new array, called updatedMealsDescription, otherwise it just copies the full object literal, unchanged, into the new array
+function updateDescription (meal) {
+    if (meal.id === 2) {
+        return {
+            ...meal,
+            description: 'Early Lunch',
+        };
+    }
+    return meal;
+}
+
+console.log(updatedMealsDescription);
+
+//uses filter function on an array to remove an item
+//within the filter function, we are calling an anonymous function to return true if we want the item included in the new array, called filtered meals, and false if we don't. we are filtering out id = 1
+const filteredMeals = updatedMeals.filter(function(meal){
+    return meal.id !== 1;
+});
+
+console.log(filteredMeals);
