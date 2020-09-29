3cf37f0496abb1dfe61275d34c756e6edd2fa8c8
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-09-29 11:29:17 -0400

    20200929

--- JavaScript_Programing/scratchPad.js
+++ JavaScript_Programing/scratchPad.js
@@ -1,41 +1,42 @@
-//use map function to update an item in an immutable array
-const meals = [
-    {id: 1, description: 'Breakfast', calories: 420},
-    {id: 2, description: 'Lunch', calories: 520},
-];
+//find sum of all the items in the array
 
-const newMeal = {
-    id: 3,
-    description: 'Snack',
-    calories: 180,
-};
+const grades = [60, 55, 80, 90, 99, 92, 75, 72];
 
-//... before meals means copy all the data from meals into new object literal with newMeal info as well
-const updatedMeals = [...meals, newMeal];
-
-console.log(meals, updatedMeals);
-
-//creates a new object literal, called updatedMealsDescription, by iterating over each index in the updatedMeals array and running the updateDescription function at each point
-const updatedMealsDescription = updatedMeals.map(updateDescription);
-
-
-//looks at the id of the object literal in the array at each index of a matrix, and if the id is 2, it updates the description and copies it into a new array, called updatedMealsDescription, otherwise it just copies the full object literal, unchanged, into the new array
-function updateDescription (meal) {
-    if (meal.id === 2) {
-        return {
-            ...meal,
-            description: 'Early Lunch',
-        };
-    }
-    return meal;
+//pure function that takes two arguments and returns their sum
+function sum (acc, grade) {
+    return acc + grade;
 }
 
-console.log(updatedMealsDescription);
+//uses reduce to map over the numbers array and sum function is passed in as argument
+//therefore sum function takes in first two items of the array, which are 60 & 55, and sums them to 115, which is returned
+//the returned value of 115 becomes the acc and the next element in the array becomes the grade in the next round of the sum function
+//the acc argument in this type of function is often called the accumulator, hence the choice in variable name
+const total = grades.reduce(sum);
 
-//uses filter function on an array to remove an item
-//within the filter function, we are calling an anonymous function to return true if we want the item included in the new array, called filtered meals, and false if we don't. we are filtering out id = 1
-const filteredMeals = updatedMeals.filter(function(meal){
-    return meal.id !== 1;
-});
+//gives the number of elements in the grades array by counting its length
+const count = grades.length;
 
-console.log(filteredMeals);
+//we can pass in an initial value of the acc by giving the groupByGrade an argument when we call it, and it doesn't have to be a number
+//we are going to pass an empty object literal ({}) as a second argument which reduce function will begin to fill with grades
+function groupByGrade (acc, grade) {
+    //destructure acc values into an object we can use to count the number of each letter grade and initialize it so that we never have to deal with a null
+    const {a = 0, b = 0, c = 0, d = 0, f = 0} = acc;
+    if (grade >= 90) {
+        //returns an object with the value of the accumulator spread into it and increments the number of A's if any
+        return { ...acc, a: a + 1};
+    } else if (grade >= 80) {
+        return { ...acc, b: b + 1};
+    } else if (grade >= 70) {
+        return { ...acc, c: c + 1};
+    } else if (grade >= 60) {
+        return { ...acc, d: d + 1};
+    } else {
+        return { ...acc, f: f + 1};
+    }
+}
+
+//maps over the grades array and runs the groupByGrade function
+//passes an empty object literal as the second argument
+const letterGradeCount = grades.reduce(groupByGrade, {});
+
+console.log(total, count, total/count, letterGradeCount);
