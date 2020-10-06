4b1dea0fb9a5ae001ed290956dd277c93e8b78b3
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-10-05 20:06:16 -0400

    20201005

--- C++_Programing/scratchPad.cpp
+++ C++_Programing/scratchPad.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 
 int main() {
     
-    int favNum;
-    
-    std::cout << "Enter your favorite number between 0-100: " << std::endl;
-
-    //takes input from user through the terminal
-    std::cin >> favNum; 
-
-    std::cout << "Wow! That's my favorite number too!" << std::endl;
-
-    std::cout << "No, " << favNum << " really is my favorite." << std::endl;
+    int age;
+    cout << age << endl;
 
     return 0;
 }
