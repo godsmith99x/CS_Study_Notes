6fb997f3b03914643bdb293aae1b12d34c08137b
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-10-08 18:40:28 -0400

    20201008

--- C++_Programing/scratchPad.cpp
+++ C++_Programing/scratchPad.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <climits>
+#include <cfloat>
 
 using namespace std;
 
 
 int main() {
-    
-    int age;
-    cout << age << endl;
 
-    return 0;
+	cout << "char: " << sizeof(char) << " bytes" << endl;	
+	cout << "int: " << sizeof(int) << " bytes" << endl;
+	cout << "unsigned int: " << sizeof(unsigned int) << " bytes" << endl;
+	cout << "short: " << sizeof(short) << " bytes" << endl;
+	cout << "long: " << sizeof(long) << " bytes" << endl;
+	cout << "long long: " << sizeof(long long) << " bytes" << endl;
+
+	return 0;
 }
-
-
