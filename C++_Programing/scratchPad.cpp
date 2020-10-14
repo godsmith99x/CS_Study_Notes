7094cf0b304d526efeff90f74c9f957e9e1e7204
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-10-14 17:08:44 -0400

    20201014

--- C++_Programing/scratchPad.cpp
+++ C++_Programing/scratchPad.cpp
@@ -14,5 +14,39 @@ int main() {
 	cout << "long: " << sizeof(long) << " bytes" << endl;
 	cout << "long long: " << sizeof(long long) << " bytes" << endl;
 
+	cout << "==============================" << endl;
+
+	cout << "float: " << sizeof(float) << " bytes" << endl;
+	cout << "double: " << sizeof(double) << " bytes" << endl;
+	cout << "long double: " << sizeof(long double) << " bytes" << endl;
+
+	cout << "==============================" << endl;
+
+	cout << "Minimum values:" << endl;
+	cout << "char: " << CHAR_MIN << endl;
+	cout << "int: " << INT_MIN << endl;
+	cout << "short: " << SHRT_MIN << endl;
+	cout << "long: " << LONG_MIN << endl;
+	cout << "long long: " << LONG_LONG_MIN << endl;
+
+	cout << "==============================" << endl;
+
+	cout << "Maximum values:" << endl;
+	cout << "char: " << CHAR_MAX << endl;
+	cout << "int: " << INT_MAX << endl;
+	cout << "short: " << SHRT_MAX << endl;
+	cout << "long: " << LONG_MAX << endl;
+	cout << "long long: " << LONG_LONG_MAX << endl;
+
+	cout << "==============================" << endl;
+
+	cout << "sizeof using variable names: " << endl;
+	int age {21};
+	cout << "age is: " << sizeof(age) << " bytes" << endl;
+	
+	double wage {22.24};
+	cout << "wage is: " << sizeof(wage) << " bytes" << endl;
+
+
 	return 0;
 }
