b5553c85530904ad6c1cabaac45998dad196139c
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-11-03 14:51:42 -0500

    20201103

---
+++ C++_Programing/UFO_Game/ufo.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() 
+{
+  std::string codeword {"codecademy"};
+  std::string answer {"__________"};
+  int misses {0};
+
+  greet();
+
+  std::vector<char> incorrect {};
+
+  bool guess {false};
+
+  char letter {};
+  
+  while (answer != codeword && misses < 7) 
+  {
+    display_misses(misses);
+
+    display_status(incorrect, answer);
+
+    std::cout << "Please enter your guess: ";
+    std::cin >> letter;
+
+    for (auto& eachLetter : codeword)
+    {
+      if (letter == eachLetter)
+      {
+        answer[eachLetter] = letter;
+        guess = true;
+      }
+    }
+
+    if (guess == true) 
+    {
+      std::cout << "\n\nCorrect!" << std::endl;
+    } else
+    {
+      std::cout << "\n\nIncorrect! The tractor beam pulls the person in further." << std::endl;
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+    guess = false;
+
+  }
+
+  std::cout << std::endl;
+
+  end_game(answer, codeword);
+  
+  
+}
+
