01c0d0b9fc70e47a088cb63eb34a02dc35538eb9
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-11-04 22:38:22 -0500

    20201104

--- C++_Programing/TicTacToe_Game/ttt.cpp
+++ C++_Programing/TicTacToe_Game/ttt.cpp
@@ -7,7 +7,41 @@ using namespace std;
 int main()
 {
 
-    vector
+    const vector<vector<char>> startingGameBoard 
+    {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    vector<vector<char>> playingGameBoard
+    {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    vector<vector<char>> player1
+    {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    vector<vector<char>> player2
+    {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    bool gameInPlay {true};
+
+    while(gameInPlay == true)
+    {
+
+        gameInPlay = false;
+    }
     
     return 0;
 }
