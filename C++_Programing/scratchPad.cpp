22c473926ad1aa0d7eb5795aee0afea60d399deb
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-12-08 20:43:39 -0500

    20201208

--- C++_Programing/scratchPad.cpp
+++ C++_Programing/scratchPad.cpp
@@ -1,37 +1,13 @@
-#include <vector>
-#include <string>
-#include <memory>
 
-class Player
+
+int main()
 {
-private:
-  std::string name;
-  int health;
-  int xp;
 
-public:
-  void talk(std::string);
-  bool isDead();
-};
+  int soda {99};
 
+  int& pop {soda};
 
+  pop += 1;
 
-int main() 
-{
-  //creates a frank and a hero object on the stack
-  Player frank;
-  Player hero;
-
-  //creates a vector of player objects called players
-  std::vector<Player> players {frank};
-
-  players.push_back(hero);
-
-  //uses a smart pointer to dynamically allocate an enemy object on the heap
-  std::unique_ptr<Player> enemy {new Player};
-
-  enemy -> talk("Fuck you!");
-
-  
   return 0;
 }
