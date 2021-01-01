7271aa2f4c12a1757ccba50a62e7820c0f2512a1
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2020-12-31 23:19:48 -0500

    20201231

---
+++ C++_Programing/Cherno/Header_Files/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void Log(const char* message)
+{
+    std::cout << message << std::endl;
+}
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+}
