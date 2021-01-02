2410b9b10224852091fb4cb247581fdb28513de1
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-02 17:14:03 -0500

    20210102

--- C++_Programing/Cherno/Static_Example/main.cpp
+++ C++_Programing/Cherno/Static_Example/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+struct Entity
+{
+    int x, y;
+
+    void Print() const
+    {
+        std::cout << x << " , " << y << std::endl;
+    }
+
+};
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Entity e{};
+    e.x = 2;
+    e.y = 3;
+
+    Entity e1 = {5, 8};
+
+    e.Print();
+    e1.Print();
+
+    std::cin.get();
+
     return 0;
 }
