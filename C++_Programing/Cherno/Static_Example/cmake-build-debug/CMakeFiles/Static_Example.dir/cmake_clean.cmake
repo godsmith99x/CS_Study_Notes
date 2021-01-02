baf03dcdd15b84bd088826626f75b9d6a0797f67
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-02 17:05:01 -0500

    20210102

---
+++ C++_Programing/Cherno/Static_Example/cmake-build-debug/CMakeFiles/Static_Example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Static_Example.dir/main.cpp.o"
+  "Static_Example"
+  "Static_Example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Static_Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
