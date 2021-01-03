2517f1e14f09db175757e95c04b23ab72519229b
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-03 16:36:54 -0500

    20210103

---
+++ C++_Programing/Cherno/Enum_Practice/cmake-build-debug/CMakeFiles/Enum_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Enum_Practice.dir/main.cpp.o"
+  "Enum_Practice"
+  "Enum_Practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Enum_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
