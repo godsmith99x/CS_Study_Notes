a6ee571bbfdac0dc214f80cdf41e0e3d0ec24f7a
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-03 18:47:20 -0500

    20210103

--- C++_Programing/Cherno/Log_Class/main.cpp
+++ C++_Programing/Cherno/Log_Class/main.cpp
@@ -1,7 +1,58 @@
+//
+// Created by joel on 1/2/21.
+//
+
 #include <iostream>
+
+class Log
+{
+public:
+
+    enum Level : unsigned char
+    {
+        Error = 0, Warning, Info
+    };
+
+private:
+    Level m_LogLevel = Info;
+public:
+    void SetLevel(Level level)
+    {
+        m_LogLevel = level;
+    }
+
+    void ErrorMessage(const char* message) const
+    {
+        if (m_LogLevel >= Error)
+        {
+            std::cout << "[Error] " << message << std::endl;
+        }
+    }
+
+    void WarnMessage(const char* message) const
+    {
+        if (m_LogLevel >= Warning)
+        {
+            std::cout << "[WARNING]: " << message << std::endl;
+        }
+    }
+
+    void InfoMessage(const char* message) const
+    {
+        if (m_LogLevel >= Info)
+        {
+            std::cout << "[Info]: " << message << std::endl;
+        }
+    }
+
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Log log;
+    log.SetLevel(Log::Error);
+    log.WarnMessage("Hello");
+    log.ErrorMessage("Hello");
+    log.InfoMessage("Hello");
+    std::cin.get();
 }
