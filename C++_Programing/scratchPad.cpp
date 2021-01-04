0a697f0b23f32f8a87b19e5313903bbdffd4fc87
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-03 19:28:38 -0500

    20210103

--- C++_Programing/scratchPad.cpp
+++ C++_Programing/scratchPad.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include <vector>
+
+class Entity
+{
+public:
+  float X, Y;
+
+  Entity()
+  {
+      X = 0.0f;
+      Y = 0.0f;
+  }
+
+  void Print()
+  {
+    std::cout << X << ", " << Y << std::endl;
+  }
+};
 
 int main()
 {
 
-  
-  return 0;
+  Entity e;
+  std::cout << e.X << std::endl;
+  e.Print();
+  std::cin.get();
 }
