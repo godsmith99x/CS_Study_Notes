3dd317831125c643446acd097fff586872b30f7d
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-13 19:53:23 -0500

    20210113

---
+++ C++_Programing/Recursion_Practice/cmake-build-debug/CMakeFiles/Recursion_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recursion_Practice.dir/main.cpp.o"
+  "Recursion_Practice"
+  "Recursion_Practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recursion_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
