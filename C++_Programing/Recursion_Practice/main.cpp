2dc553e7f9444a17afa36090a8b04dec86f9e85f
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-13 20:24:18 -0500

    20210113

--- C++_Programing/Recursion_Practice/main.cpp
+++ C++_Programing/Recursion_Practice/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+void tail(int n)
+{
+    if (0 == n)
+        return;
+
+    std::cout << "Tail function called with n= " << n << std::endl;
+
+    tail(n - 1);
+}
+
+void head(int n)
+{
+    if (0 == n)
+        return;
+
+    head(n - 1);
+
+    std::cout << "Head function called with n= " << n << std::endl;
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    tail(10);
+
+    std::cout << std::endl;
+    std::cout << "----------------" << std::endl;
+    std::cout << std::endl;
+
+    head(10);
+
+    std::cin.get();
+
     return 0;
 }
