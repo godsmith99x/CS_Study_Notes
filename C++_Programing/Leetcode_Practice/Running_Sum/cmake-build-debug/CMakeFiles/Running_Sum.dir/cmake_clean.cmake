e9796d97561ef94f8d2b84d77d96453a4b4ffcfa
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-19 22:07:07 -0500

    20210119

---
+++ C++_Programing/Leetcode_Practice/Running_Sum/cmake-build-debug/CMakeFiles/Running_Sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Running_Sum.dir/main.cpp.o"
+  "Running_Sum"
+  "Running_Sum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Running_Sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++_Programing/Leetcode_Practice/Running_Sum/cmake-build-debug/CMakeFiles/Running_Sum.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Running_Sum.
+# This may be replaced when dependencies are built.
