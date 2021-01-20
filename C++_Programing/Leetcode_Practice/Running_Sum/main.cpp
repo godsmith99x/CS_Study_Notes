5cec10ee255ef7c99660085115ade5fed9052984
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-19 22:56:09 -0500

    20210119

--- C++_Programing/Leetcode_Practice/Running_Sum/main.cpp
+++ C++_Programing/Leetcode_Practice/Running_Sum/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
+
+
+std::vector<int> runningSum(std::vector<int>& nums);
+
+void print(std::vector <int> const &a);
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> nums1 = {1,2,3,4};
+    std::vector<int> nums2 = {1,1,1,1,1};
+    std::vector<int> nums3 = {3,1,2,10,1};
+
+    print(runningSum(nums1));
+    print(runningSum(nums2));
+    print(runningSum(nums3));
     return 0;
+}
+
+
+std::vector<int> runningSum(std::vector<int>& nums)
+{
+    std::vector<int> v (nums.size());
+    std::partial_sum(nums.begin(), nums.end(), v.begin());
+    return v;
+}
+
+void print(std::vector <int> const &a)
+{
+    std::cout << "The vector elements are : ";
+
+    for(int i : a)
+    {
+        std::cout << i << ' ';
+    }
+
+    std::cout << std::endl;
 }
