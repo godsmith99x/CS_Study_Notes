5a2c87bae6c05bd51e44ad9dd1a62242b9eb0d89
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-27 21:04:30 -0500

    Annotated reasons for code choices in simple string class.

--- C++_Programing/Cherno/Move_Semantics/main.cpp
+++ C++_Programing/Cherno/Move_Semantics/main.cpp
@@ -1,53 +1,70 @@
 #include <iostream>
 #include <cstring>
 
-//basic string class just for example of copying and moving
+//Basic string class just for example of copying and moving
+//TODO:need to be null terminated
 class String
 {
 public:
+    //1. Default constructor
     String() = default;
 
-    String(const char* string)
+    //2. String constructed by passing in an argument of a series of  characters through a const pointer to array of
+    // char-m_Sized block of memory on the heap.
+    explicit String(const char* string)
     {
         printf("Created!\n");
+        //2.a Uses strlen() to calculate length of the string passed in to create the size of the array
+        // of characters.
         m_Size = strlen(string);
+
+        //2.b Creates the space on the heap for memcpy() to copy in the string.
         m_Data = new char[m_Size];
         memcpy(m_Data, string, m_Size);
     }
 
-    //Copy constructor
+    //3. Copy constructor, creates a copy of a string in a new location on the heap.
     String(const String &other)
     {
         printf("Copied!\n");
+        //3.a Sets the size for the array on the heap to the size of other.m_Size property.
         m_Size = other.m_Size;
+
+        //3.b Creates the new array on the heap and memcpy() copies in the other.m_Data property.
         m_Data = new char[m_Size];
         memcpy(m_Data, other.m_Data, m_Size);
     }
 
+    //4. Destructor
     ~String()
     {
+        //4.a Deletes this instance's data from the heap.
         delete m_Data;
     }
 
+    //5. Public method to print the string.
     void Print()
     {
+        //5.a Loop through each uint32_t sized index in the array.
         for (uint32_t i = 0; i < m_Size; ++i)
         {
+            //5.b printf() the character at that index in the array.
             printf("%c", m_Data[i]);
         }
         printf("\n");
     }
 
 private:
-    char* m_Data;
-    uint32_t m_Size;
+    //6. Private variables to store a pointer to the data and the size of the array.
+    char* m_Data{};
+    uint32_t m_Size{};
 };
 
 //Entity class to consume string
 class Entity
 {
 public:
-    Entity(const String &name)
+    explicit Entity(const String &name)
             : m_Name(name)
     {
 
@@ -65,6 +82,8 @@ private:
 
 int main()
 {
+    //The explicit cast to String was done for clarity and teaching. The cast to from String to Entity would be
+    // implied by the compiler if String were omitted.
     Entity entity(String("Joel"));
     entity.PrintName();
 
